c924cecf01a66c574d6a1f98af189489706ca6c9
motty93 <rdwbocungelt5@gmail.com>
2018-09-08 23:00:58 +0900

    ぼっち演算子

---
+++ PerfectRuby/lonely_operator.rb
@@ -0,0 +1,12 @@
+# ぼっち演算子
+
+people = %w(alice bob carol)
+
+person0 = people[0]
+person9 = people[9]  #nilになる
+
+person0&.capitalize
+person0.capitalize
+
+person9&.capitalize   # nil
+person9.capitalize  #NoMethoeErrorになる
