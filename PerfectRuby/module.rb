8c90236ddcd7e45004bea4a7c0fcdde512a6c16f
motty93 <rdwbocungelt5@gmail.com>
2018-09-24 23:20:19 +0900

    モジュールに関して

---
+++ PerfectRuby/module.rb
@@ -0,0 +1,14 @@
+module Sweet
+  def self.lot
+    %w(brownie apple-pie bavarois pudding).sample
+  end
+
+  module Chocolate; end
+
+  module Brownie; end
+end
+
+puts Sweet.lot
+puts Sweet.lot
+p Sweet::Chocolate
+p Sweet::Brownie
