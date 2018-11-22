f0be34cb69c6f66b6152f7fcc410dc74c4d1a444
motty93 <rdwbocungelt5@gmail.com>
2018-11-22 10:41:08 +0900

    takeメソッド & detectメソッド等

--- PerfectRuby/ClassModule/enumerable.rb
+++ PerfectRuby/ClassModule/enumerable.rb
@@ -35,3 +35,25 @@ end
 # １つでも真の要素があればtrue
 [true, true, true].one?
 [false, false, true].one?
+
+# 部分的な要素の取得
+%w(Alice Bob Charlie).grep(/a/i)
+['a', 1, 'b', 2].grep(String)
+['a', 1, 'b', 2].grep(Integer)
+['a', 1, 'b', 2, []].grep(Array).flatten
+['a', 1, 'b', 2, [], {}, {a: 1, b: 2}].grep(Hash)
+['a', 1, 'b', 2, [], {}, {a: 1, b: 2}].grep(Hash).select(&:any?)
+
+# マッチした最初の要素を取得
+[4, 4, 2, 3].detect(&:even?)
+[4, 4, 2, 3].find(&:even?)
+
+# 任意の数の要素を返す
+[4, 2, 4, 1].take(3)
+# 任意の数の要素をスキップして残りの要素を返す
+[4, 2, 4, 1].drop(2)
+
+# 偽を返すまで任意の数の要素を返す
+[4, 2, 4, 1].take_while { |n| n < 3 }
+# 偽を返すまで任意の数の要素をスキップして残りの要素を返す
+[4, 2, 4, 1].drop_while { |n| n < 3 }
