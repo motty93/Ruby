451f439f5571620e22189811e93c01c6fad7a7e2
motty93 <rdwbocungelt5@gmail.com>
2018-11-24 23:59:40 +0900

    配列の取得方法

---
+++ PerfectRuby/ClassModule/get_array.rb
@@ -0,0 +1,12 @@
+array = [4, 4, 2, 3]
+# Array#values_at 複数の添字の要素を配列で返す
+array.values_at(1)
+array.values_at(1, 3)
+
+# ランダムで要素を返す
+array.sample(2)
+array.sample
+
+# 最初と最後を返す
+array.last
+array.first
