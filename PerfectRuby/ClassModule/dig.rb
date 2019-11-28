33d715262bc63033eade3bb3db99b036db6bccb0
motty93 <rdwbocungelt5@gmail.com>
2019-11-29 00:22:06 +0900

    add: Array#dig Hash#dig railsのparams取得に使える

---
+++ PerfectRuby/ClassModule/dig.rb
@@ -0,0 +1,14 @@
+# railsでparamsを取得する際に使えそう
+
+hash = {
+  alice: { books: { wonder: 1234 }},
+  charlie: {}
+}
+
+hash[:charlie][:books][:chocolate] #=> NoMethodError
+hash.dig(:alice, :books, :wonder) #=> nil
+
+
+hash = { a: [{ word: 'alice' }, { word: 'alpha' }] }
+hash.dif(:a, 0, :word) #=> 'alice'
+
